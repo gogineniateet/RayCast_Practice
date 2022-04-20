d191fb715d9fb1b0e8acbcc154b547a55292ed98
AteetGogineni <ateet.gogineni@yesgnome.com>
2022-04-20 12:46:53 +0530

    created obatacle, destroying obstacle using raycast hit....

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     CharacterController controller;
     public float playerSpeed;
+    public Transform rayPoint;
 
 
     // Start is called before the first frame update
@@ -22,5 +23,22 @@ public class PlayerController : MonoBehaviour
 
         Vector3 move = transform.right * inputX + transform.forward * inoutZ;
         controller.Move(move * playerSpeed * Time.deltaTime);
+        if(Input.GetMouseButtonDown(0))
+        {
+            Hit();
+        }
+    }
+
+    private void Hit()
+    {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(rayPoint.position, rayPoint.forward, out hitInfo, 100f))
+        {
+            GameObject hitZombie = hitInfo.collider.gameObject;
+            if (hitZombie.tag == "Obstacle")
+            {
+                Destroy(hitZombie);
+            }
+        }
     }
 }
